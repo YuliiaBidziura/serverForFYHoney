ab733480a3635358ac73689edee75094addb4a09
YuliiaBidziura <90046465+YuliiaBidziura@users.noreply.github.com>
2021-12-21 18:43:54 +0200

    Adress added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 const DB_URL = 'mongodb+srv://YuliiaBidziura:UsEr123@cluster0.xp0v5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://yuliiabidziura.github.io/FYHoney/',
     optionsSuccessStatus: 200,
 }
 
